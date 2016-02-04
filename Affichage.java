73ef7abb984d72caddf16554c15132284bc7f696
vincentgosselin1 <vincentgosselin1@gmail.com>
2016-02-04 12:04:14 -0500

    Update Affichage.java

--- Affichage.java
+++ Affichage.java
@@ -1,3 +1,4 @@
+
 package labo1;
 
 import java.awt.*;
@@ -9,9 +10,8 @@ public class Affichage extends JFrame{
 	private MyCanvas canvas = new MyCanvas();
 	private Forme f = null;//Prochaine Forme a dessiner!
 	
-	private Forme t[] = new Forme[10];//Tableau de 10 formes.
-	private int i = 0;//Index pour fill le tableau de formes.
-	
+	private Forme[] listeFormes = new Forme[10];
+	int i =0;
 	
 	//Couleur Aleatoire!
 	Random nbAleatoire = new Random();
@@ -21,6 +21,8 @@ public class Affichage extends JFrame{
 	{
 		Affichage A1 = new Affichage();
 		Forme F1 = new Carre("","50","50","100","200");
+		A1.afficherForme(F1);
+		F1 = new Carre("","200","200","60","60");
 		A1.afficherForme(F1);
 	}
 	
@@ -38,22 +40,31 @@ public class Affichage extends JFrame{
 	
 	public void afficherForme(Forme F1)
 	{
-		t[i] = F1;//Fill le tableau de formes.
-		f = t[i];
-		canvas.repaint();//On dessine les formes dans le tableau.
-		i++;
+		 listeFormes[i] = F1;
+		 i++;
+		 if(i>9)
+		 {
+			 i=0;
+		 }
+		 repaint();//On dessine les formes dans le tableau.
 	}
 	
-	private class MyCanvas extends Canvas
+	private class MyCanvas extends JPanel
 	{	
-		public void update(Graphics g)
+		public void paintComponent(Graphics g)
 		{
-				if(f != null)//Si il y a une forme a dessiner.
+			super.paintComponent(g);
+			
+			for(int y = 0; y<10;y++)
+			{
+				
+				if( listeFormes[y] != null)//Si il y a une forme a dessiner.
 				{
+					f = listeFormes[y];
 						switch(f.nom)
 						{
-						case "CARRE" : g.setColor(Color.BLACK);	g.drawRect(f.p1, f.p2, f.p3, f.p4);	g.fillRect(f.p1, f.p2, f.p3, f.p4);		break;
-						case "RECTANGLE" : g.setColor(Color.BLUE);	g.drawRect(f.p1, f.p2, f.p3, f.p4);	g.fillRect(f.p1, f.p2, f.p3, f.p4);	break;
+						case "CARRE" : g.setColor(Color.BLACK);	g.drawRect(f.p1, f.p2, f.p3-f.p1, f.p4-f.p2);	g.fillRect(f.p1, f.p2, f.p3-f.p1, f.p4-f.p2);		break;
+						case "RECTANGLE" : g.setColor(Color.BLUE);	g.drawRect(f.p1, f.p2, f.p3-f.p1, f.p4-f.p2);	g.fillRect(f.p1, f.p2, f.p3-f.p1, f.p4-f.p2);	break;
 						case "OVALE" : g.setColor(Color.CYAN); g.drawOval(f.p1, f.p2, f.p3, f.p4); g.fillOval(f.p1, f.p2, f.p3, f.p4);		break;
 						case "CERCLE" : g.setColor(Color.GREEN); g.drawOval(f.p1, f.p2, f.p3, f.p3); g.fillOval(f.p1, f.p2, f.p3, f.p3);		break;
 						case "LIGNE" : g.setColor(Color.RED); g.drawLine(f.p1, f.p2, f.p3, f.p4);	break;
@@ -62,6 +73,5 @@ public class Affichage extends JFrame{
 					
 		}
 	}
+	}
 }
-
-
